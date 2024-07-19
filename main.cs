2e3a85819c807f0a50403ae35656bf649fef1b02
ASiddiqi2004 <amaansiddiqi@usf.edu>
2024-07-19 04:16:38 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program {
+  static void Main(string[] args)
+    {
+        Console.WriteLine("Enter message:");
+        string message = Console.ReadLine();
+
+        int length = message.Length;
+
+        if (length > 140)
+        {
+            Console.WriteLine("Too long, please shorten to below 140 words.");
+        }
+        else
+        {
+            Console.WriteLine("This message is elligble.");
+        }
+    }
+  }
